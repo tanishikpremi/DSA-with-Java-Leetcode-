2510f95312ddddef5cc155f07fcbdce550450485
tanishikpremi <tanishikpremi@gmail.com>
2026-07-14 00:20:03 -0700

    Time: 0 ms (100%), Space: 48.3 MB (50.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+
+        int low = 0;
+         int high =nums.length -1;
+
+        while(low <= high){
+
+            int middle = low + (high - low)/2;
+            int value = nums[middle];
+
+            if(value < target) low = middle +1;
+            else if(value > target) high = middle -1;
+            else return middle;
+
+        }
+        return -1;
+        
+    }
+}
